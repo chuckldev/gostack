a660883069eca438b301ff2b3079a7c18d185b90
chuckldev <chuckldev@outlook.com>
2022-12-20 11:51:10 +0000

    final stack

---
+++ stack.go
@@ -0,0 +1,36 @@
+package stack
+
+type Stack struct {
+	Data []interface{}
+}
+
+func New() *Stack {
+	return &Stack{nil}
+}
+
+func (s *Stack) Push(data interface{}) {
+	s.Data = append(s.Data, data)
+}
+
+func (s *Stack) Pop() interface{} {
+	res := s.Data[len(s.Data)-1 : len(s.Data)]
+	s.Data = s.Data[:len(s.Data)-1]
+	return res[0]
+}
+
+func (s *Stack) Size() int {
+	return len(s.Data)
+}
+
+func (s *Stack) IsEmpty() bool {
+	if len(s.Data) == 0 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func (s *Stack) Peek() interface{} {
+	res := s.Data[len(s.Data)-1 : len(s.Data)]
+	return res[0]
+}
---
+++ stack_test.go
@@ -0,0 +1,146 @@
+package stack
+
+import (
+	"testing"
+)
+
+type TestStruct struct {
+	Name  string
+	Value int
+}
+
+func TestPush(t *testing.T) {
+	stk := New()
+
+	if len(stk.Data) != 0 {
+		t.Errorf("Stack should be zero ( 0 )")
+	}
+
+	stk.Push(TestStruct{Name: "number", Value: 1})
+
+	if len(stk.Data) != 1 {
+		t.Errorf("Stack should be one ( 1 )")
+	}
+
+	stk.Push(2)
+
+	if len(stk.Data) != 2 {
+		t.Errorf("Stack should be two ( 2 )")
+	}
+}
+
+func TestPop(t *testing.T) {
+	stk := New()
+	wantName := "number"
+	wantVal := 1
+
+	if len(stk.Data) != 0 {
+		t.Errorf("Stack should be zero ( 0 )")
+	}
+
+	stk.Push(5)
+
+	if len(stk.Data) != 1 {
+		t.Errorf("Stack should be one ( 1 )")
+	}
+
+	stk.Push(TestStruct{Name: "number", Value: 1})
+
+	if len(stk.Data) != 2 {
+		t.Errorf("Stack should be two ( 2 )")
+	}
+
+	val := stk.Pop().(TestStruct)
+
+	if len(stk.Data) != 1 {
+		t.Errorf("Stack should be one ( 1 )")
+	}
+
+	if val.Name != wantName {
+		t.Errorf("val %s does not match %s", val.Name, wantName)
+	}
+
+	if val.Value != wantVal {
+		t.Errorf("val %d does not match %d", val.Value, wantVal)
+	}
+}
+
+func TestSize(t *testing.T) {
+	stk := New()
+	want := 2
+
+	if len(stk.Data) != 0 {
+		t.Errorf("Stack should be zero ( 0 )")
+	}
+
+	stk.Push(TestStruct{Name: "number", Value: 1})
+
+	if len(stk.Data) != 1 {
+		t.Errorf("Stack should be one ( 1 )")
+	}
+
+	stk.Push(2)
+
+	if len(stk.Data) != 2 {
+		t.Errorf("Stack should be two ( 2 )")
+	}
+
+	if stk.Size() != want {
+		t.Errorf("Stack Size() should be two ( %d )", want)
+	}
+}
+
+func TestIsEmpty(t *testing.T) {
+	stk := New()
+	want := true
+
+	if stk.IsEmpty() != want {
+		t.Errorf("Stack IsEmpty() should be %t", want)
+	}
+
+	stk.Push(1)
+	want = false
+
+	if stk.IsEmpty() != want {
+		t.Errorf("Stack IsEmpty() should be %t", want)
+	}
+}
+
+func TestPek(t *testing.T) {
+	stk := New()
+	wantName := "number"
+	wantVal := 1
+
+	if len(stk.Data) != 0 {
+		t.Errorf("Stack should be zero ( 0 )")
+	}
+
+	stk.Push(5)
+
+	val := stk.Peek().(int)
+
+	if val != 5 {
+		t.Errorf("Stack Peek() should be five ( 5 )")
+	}
+
+	stk.Push(TestStruct{Name: "number", Value: 1})
+
+	valPeek := stk.Peek().(TestStruct)
+
+	if valPeek.Name != wantName {
+		t.Errorf("val %s does not match %s", valPeek.Name, wantName)
+	}
+
+	if valPeek.Value != wantVal {
+		t.Errorf("val %d does not match %d", valPeek.Value, wantVal)
+	}
+
+	stk.Pop()
+
+	val = stk.Peek().(int)
+
+	if val != 5 {
+		t.Errorf("Stack Peek() should be five ( 5 )")
+	}
+
+}
